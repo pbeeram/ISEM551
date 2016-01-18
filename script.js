6569e457bd44c8fc74b9eab7a4877ca68b086b37
pbeeram <praveenreddy.beeram@gmail.com>
2016-01-17 19:39:46 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var sampleApp = angular.module('phonecatApp', []);
+  
+sampleApp .config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/addOrder', {
+        templateUrl: 'templates/add-order.html',
+        controller: 'AddOrderController'
+      }).
+      when('/showOrders', {
+        templateUrl: 'templates/show-orders.html',
+        controller: 'ShowOrdersController'
+      }).
+      otherwise({
+        redirectTo: '/addOrder'
+      });
+  }]);

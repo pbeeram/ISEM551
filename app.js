4752cafcfe6112119e76b5f45f0e55817f4fe33e
pbeeram <praveenreddy.beeram@gmail.com>
2016-01-17 19:44:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-//Define an angular module for our app
+
 var sampleApp = angular.module('sampleApp', []);
  
-//Define Routing for app
-//Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
-//Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
 sampleApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
